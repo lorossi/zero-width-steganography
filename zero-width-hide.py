18bd4c65f615068734c2dc01582391c6a98e29e3
lorossi <lo.rossi@live.com>
2020-06-26 16:42:46 +0200

    added --verbose and --version, commented code, general refactoring

--- zero-width-hide.py
+++ zero-width-hide.py
@@ -11,7 +11,8 @@ def main():
                         filemode="w")
 
     parser = argparse.ArgumentParser(description="Hide and recover text in plainsight", epilog="You will find a log file with the same name as the script. If any error is raised, check there.")
-
+    parser.add_argument('-V', '--version', help='library version', action='store_true')
+    parser.add_argument('-v', '--verbose', help='verbose mode. If activated it will only prompt errors', action='store_true')
     parser.add_argument('-s', '--source', help='source file', type=str)
     parser.add_argument('-d', '--destination', help='destination file - leave empty to output to console', required=False, type=str)
     parser.add_argument('-S', '--sourcetext', help='source text', type=str, required=False)
@@ -21,9 +22,15 @@ def main():
     parser.add_argument('-t', '--textpath', help='path of a clear text file to be encoded string', required=False, type=str)
     parser.add_argument('-C', '--clean', help='clean source file from hidden text', required=False, action="store_true")
     parser.add_argument('-p', '--position', help='set position of encoded text in destination file. Can be: a list of line numbers, "random", "top", "bottom", "lines", "nth". Defaults to "top"', required=False, default="top")
-    parser.add_argument('-l', '--lines', help='set lines of encoded text in destination file if -p parameter is "lines". Defaults to 1', required=False, type=int, nargs="+", default=[0])
+    parser.add_argument('-l', '--lines', help='set lines of encoded text in destination file if -p parameter is "lines". Defaults to first.', required=False, type=int, nargs="+", default=1)
     parser.add_argument('-o', '--occasions', help='set number of lines of encoded text in destination file if -p parameter is "random", number of lines to skip if -p is "nth". Defaults to 1', required=False, type=int, nargs="+", default=[1])
     args = parser.parse_args()
+    z = zerowidth.ZeroWidth()
+
+    if (args.version):
+        version = z.version()
+        print(f"Zero width version: {version}")
+        return
 
     if not(args.decode or args.encode or args.clean):
         logging.error("No encode/decode/clean option specified or No destination and encode specified")
@@ -55,18 +62,17 @@ def main():
         parser.print_help()
         return
 
-    z = zerowidth.ZeroWidth()
-
     if (args.source):
         try:
             z.readFile(args.source)
         except Exception as e:
             logging.error(f"Error while opening source file. Error: {e}")
             print("Couldn't read source file. Look at log for more informations")
-            traceback.print_exc()
+            #traceback.print_exc()
             return
         logging.info("Source file loaded")
-        print("Source file loaded")
+        if (args.verbose):
+            print("Source file loaded")
 
     elif (args.sourcetext):
         try:
@@ -74,7 +80,7 @@ def main():
         except Exception as e:
             logging.error(f"Error while reading source text. Error: {e}")
             print("Couldn't read source text. Look at log for more informations")
-            traceback.print_exc()
+            #traceback.print_exc()
             return
         logging.info("Source text valid")
         print("Source text loaded")
@@ -82,28 +88,30 @@ def main():
     if (args.clean):
         try:
             z.searchEncodedText()
-            z.cleanFile(args.source)
+            z.cleanSource(args.source)
         except Exception as e:
             logging.error(f"Error while cleaning file. Error: {e}")
             print("Couldn't clean file. Look at log for more informations")
-            traceback.print_exc()
+            #traceback.print_exc()
             return
 
         try:
             if args.destination:
                 z.writeFile(args.destination)
                 logging.info("Destination file cleaned")
-                print("Destination file cleaned")
+                if (args.verbose):
+                    print("Destination file cleaned")
                 return
             else:
                 z.writeFile(args.source)
                 logging.info("Source file cleaned")
-                print("Source file cleaned")
+                if (args.verbose):
+                    print("Source file cleaned")
                 return
         except Exception as e:
                 logging.error(f"Error while cleaning file. Error: {e}")
                 print("Couldn't clean file. Look at log for more informations")
-                traceback.print_exc()
+                #traceback.print_exc()
                 return
 
 
@@ -118,7 +126,7 @@ def main():
         except Exception as e:
             logging.info(f"No clear text found or text couldn't be encoded. Error {e}")
             print("No clear text found or text couldn't be encoded. Look at log for more informations")
-            traceback.print_exc()
+            #traceback.print_exc()
             return
 
         try:
@@ -132,9 +140,6 @@ def main():
         logging.info("Text encoded")
 
         try:
-            """if args.source:
-                z.readFile(args.source)"""
-
             if isinstance(args.lines, int):
                 args.lines = [args.lines]
 
@@ -144,11 +149,12 @@ def main():
         except Exception as e:
             logging.info(f"No clear text found or text couldn't be encoded. Error {e}")
             print("No clear text found or text couldn't be encoded. Look at log for more informations")
-            traceback.print_exc()
+            #traceback.print_exc()
             return
 
         logging.info("Text encoded in output file")
-        print("Text encoded in output file")
+        if (args.verbose):
+            print("Text encoded in output file")
 
         return
 
@@ -158,24 +164,25 @@ def main():
 
         if not result:
             logging.info("No encoded text found")
-            print("No encoded text has been found in this file.")
+            if (args.verbose):
+                print("No encoded text has been found in this file.")
             return
 
         decoded = z.zeroDecode()
         logging.info(f"{len(decoded)} instances of text found")
-
-        decoded_string = z.zeroDecodeString()
+        decoded_string = z.zeroDecodeString(verbose=args.verbose)
 
         if (args.destination):
             try:
                 z.writeFile(args.destination)
                 logging.info("Output written to file")
-                print(f"Output written to file {args.destination}")
+                if (args.verbose):
+                    print(f"Output written to file {args.destination}")
                 return
             except Exception as e:
                 logging.error(f"Error while writing to file. Error: {e}")
                 print(f"Couldn't write to file {args.destination}. Look at log for more informations")
-                traceback.print_exc()
+                #traceback.print_exc()
                 return
         else:
             logging.info("String decoded")
--- zerowidth.py
+++ zerowidth.py
@@ -2,6 +2,7 @@ from random import randint, choice
 
 class ZeroWidth():
     def __init__(self):
+        self.version_string = "1.0"
         self.clear_text = []
         self.text_encoded = []
         self.raw = ""
@@ -13,6 +14,10 @@ class ZeroWidth():
 
         # reverses the "character_map" dict so we can map spaces to bits
         self.space_map = {v: k for k, v in self.character_map.items()}
+
+    def version(self):
+        # return the version of the library
+        return self.version_string
 
     def setClearText(self, clear_text):
         # THESE ARE THE STRINGS THAT WILL BE HIDDEN
@@ -36,6 +41,7 @@ class ZeroWidth():
         # should probably make multiple dicts? don't really know why it's a single dict
 
     def searchEncodedText(self):
+        # search for encoded text in source
         self.text_encoded = []
         linecount = 1
 
@@ -46,6 +52,7 @@ class ZeroWidth():
                 if c in list(self.space_map.keys()):
                     new_dict["text"] += c
 
+            # check if we found a multiple of 9 "bits" (a char)
             if len(new_dict["text"]) % 9 == 0 and new_dict["text"]:
                 new_dict["line"] = linecount
                 self.text_encoded.append(new_dict)
@@ -56,6 +63,7 @@ class ZeroWidth():
         return False
 
     def zeroEncode(self):
+        # encodes the text into zero width text
         self.text_encoded = []
 
         for text in self.clear_text:
@@ -75,6 +83,7 @@ class ZeroWidth():
         return self.text_encoded
 
     def zeroDecode(self):
+        # decodes the text into utf-8
         self.clear_text = []
         for text in self.text_encoded:
             new_dict = {}
@@ -90,29 +99,45 @@ class ZeroWidth():
             self.clear_text.append(new_dict)
         return self.clear_text
 
-    def zeroDecodeString(self):
+    def zeroDecodeString(self, **kwargs):
+        newl = "\n"
+        tab = "\t"
         self.decoded_string = ""
-        for dict in self.clear_text:
+
+        if kwargs["verbose"]:
+            # creates string from decoded text
             newl = "\n"
             tab = "\t"
-            output = (
-                f"Text found in line {dict['line']}:"
-                f"{tab}{dict['text'].replace(newl, ' ')}{newl}"
-            )
-            self.decoded_string += output
+            self.decoded_string += f"{len(self.clear_text)} instances of hidden text found{newl}"
+            for dict in self.clear_text:
+                output = (
+                    f"Text found in line {dict['line']}:{newl}"
+                    f"{tab}{dict['text'].replace(newl, ' ')}{newl}"
+                )
+                self.decoded_string += output
+        else:
+            for dict in self.clear_text:
+                output = (
+                    f"Line {dict['line']}:{tab}"
+                    f"{tab}{dict['text'].replace(newl, ' ')}{newl}"
+                )
+                self.decoded_string += output
 
         self.decoded_string = self.decoded_string.rstrip()
         self.output_buffer = self.decoded_string
         return self.decoded_string
 
     def readSource(self, source):
+        # reads from direct input
         self.raw = source
 
     def readFile(self, path):
+        # reads from file
         with open(path, 'r', encoding='utf-8-sig') as f:
             self.raw = f.read()
 
     def writeFile(self, path):
+        # writes to file
         with open(path, 'w',  encoding='utf-8-sig') as f:
             f.write(self.output_buffer)
 
@@ -123,8 +148,10 @@ class ZeroWidth():
     def embedEncoded(self, **kwargs):
         self.lines = self.raw.splitlines()
         if self.lines == []:
+            # we only found one empty line
             self.lines = [""]
 
+        # hides in every line (or in a list of lines)
         if kwargs["position"] == "lines":
             linecount = 0
             for p in kwargs["lines"]:
@@ -132,56 +159,71 @@ class ZeroWidth():
                 self.lines[p-1] += self.text_encoded[index]["text"]
                 linecount += 1
 
+        # hides text every nth lines
         elif kwargs["position"] == "nth":
             textcount = 0
             for i in range(1, len(self.lines) - 1, kwargs["occasions"][0]):
                 self.lines[i-1] += self.text_encoded[textcount]["text"]
                 textcount = (textcount + 1) % (len(self.text_encoded) - 1)
 
+        # hides at top of document
         elif kwargs["position"] == "top":
             self.lines[0] += self.text_encoded[-1]["all"]
 
+        # hides at bottom of document
         elif kwargs["position"] == "bottom":
             self.lines[-1] = self.text_encoded[-1]["all"] + self.lines[-1]
 
+        # hides at random positions inside document
         elif kwargs["position"] == "random":
 
             if kwargs["occasions"][0] >  len(self.lines):
+                # there are more occasions than lines
                 raise Exception("There can't be more occasions than lines. Aborting")
 
             if kwargs["occasions"][0] < len(self.text_encoded) - 2:
+                # there are more lines of text to be encoded than lines in document
                 raise Exception("There can't be less occasions than lines of text to be encoded. Aborting")
 
+            # list of free lines (as opposing to brute-forcing every line)
             available_lines = []
             for i in range(0, len(self.lines) - 1):
                 if len(self.lines[i]) > 2:
                     available_lines.append(i)
 
             if len(available_lines) < kwargs["occasions"][0]:
+                # the lines are too short to hide text
                 raise Exception("There are not enough available lines in the source file. Every line available should be at least 2 characters wide. Aborting.")
 
             current_encoded = 0
             for x in range(kwargs["occasions"][0]):
 
+                # pick a random line
                 random_line = choice(available_lines)
                 available_lines.remove(random_line)
                 current_line = self.lines[random_line]
 
+                # try to fit the text inside said line (not as first or last character)
                 found = False
                 while not found:
                     random_pos = randint(1, len(current_line) - 2)
                     if current_line[random_pos - 1] in self.space_map.keys() or current_line[random_pos + 1] in self.space_map.keys():
+                        # neighbouring charactes must not be already hidden
                         found = False
                     else:
                         found = True
 
+                # insert the encoded text in line
                 self.lines[random_line] = self.lines[random_line][:random_pos] + self.text_encoded[current_encoded]["text"] + self.lines[random_line][random_pos:]
+                # rotate to next text
                 current_encoded = (current_encoded + 1) % (len(self.text_encoded) - 1)
 
+        # text is now ready to be written
         self.output_buffer = "".join(line + '\n' for line in self.lines)
         return len(self.lines)
 
-    def cleanFile(self, path):
+    def cleanSource(self, path):
+        # remove everything from source
         if self.text_encoded == []:
             self.output_buffer =  self.raw
             return
